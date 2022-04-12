387decde057db05f58d674d7c586e5edca02d143
Oriburger <uss425@naver.com>
2022-04-13 00:22:51 +0900

    init project

---
+++ Source/GammaruStudy/GammaruStudy.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GammaruStudy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GammaruStudy, "GammaruStudy" );
